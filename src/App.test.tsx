c94bad67eebe510f2674d13857b91f173b581afa
Seb <sebastien.legallic@gmail.com>
2023-01-03 18:10:17 +0100

    🧪  Test W.I.P

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import App from "./App";
+import { Film } from "./Interfaces";
+import FilmCard from "./FilmCard";
 
-test('renders learn react link', () => {
+const fakeFilm: Film = {
+  backdrop_path: "",
+  title: "Fake title",
+  original_name: "",
+  overview: "Fake overview",
+  original_language: "fr",
+  vote_average: 4,
+};
+
+const fakeAPI = setupServer(
+  rest.get("https://api.themoviedb.org/3/search/multi", (req, res, ctx) => {
+    return res(ctx.json({ results: [fakeFilm] }));
+  })
+);
+
+beforeAll(() => fakeAPI.listen());
+afterEach(() => fakeAPI.resetHandlers());
+afterAll(() => fakeAPI.close());
+
+test("render a card of a film", () => {
+  render(<FilmCard film={fakeFilm} />);
+  const title = screen.getByText(/Fake title/i);
+  expect(title).toBeInTheDocument();
+});
+
+test("Fill in the search input and trigger search", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const searchInput = screen.getByPlaceholderText("Search a film");
+  const searchButton = screen.getByRole("searchBtn");
+
+  expect(searchInput).toBeInTheDocument();
+
+  fireEvent.change(searchInput, { target: { value: "Fake" } });
+  expect((searchInput as HTMLInputElement).value).toBe("Fake");
+
+  fireEvent.click(searchButton);
+
+  const title = screen.getByText(/Fake title/i);
+  await waitFor(() => expect(title).toBeInTheDocument());
 });
